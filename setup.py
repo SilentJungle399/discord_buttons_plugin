7fbd8f9dde28dfb9f3e4063835fa0a5aa7920bba
SilentJungle399 <75272148+SilentJungle399@users.noreply.github.com>
2021-05-28 22:12:30 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="discord_buttons_plugin",
+    version="0.1.2",
+    author="SilentJungle399",
+    description="A simple module to use for buttons in discord.py",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    url="https://github.com/SilentJungle399/discord_buttons_plugin",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
